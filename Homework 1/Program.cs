42a119ee57d45fa37e156b246fe4ee92fcaba526
Александр <87068584+Mrsuper125@users.noreply.github.com>
2023-10-01 13:37:10 +0300

    Initial, homework 1

---
+++ Homework 1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public class Shape
+{
+    private static int R;
+    private int x;
+    private int y;
+
+    static Shape()
+    {
+        R = 35;
+    }
+
+    public Shape(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public virtual void Draw()
+    {
+        Console.WriteLine($"X: {x}, Y: {y}, R: {R}");
+    }
+}
+
+class Circle : Shape
+{
+    Circle(int x, int y) : base(x, y)
+    {}
+
+    public override void Draw()
+    {
+        base.Draw();
+        Console.WriteLine("Circle");
+    }
+}
+
+class Square : Shape
+{
+    Square(int x, int y) : base(x, y)
+    {}
+
+    public override void Draw()
+    {
+        base.Draw();
+        Console.WriteLine("Suare");
+    }
+}
+
+class Triangle: Shape
+{
+    Triangle(int x, int y) : base(x, y)
+    {}
+
+    public override void Draw()
+    {
+        base.Draw();
+        Console.WriteLine("Triangle");
+    }
+}
+
+namespace Homework_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
+
+//TODO: Список вопросов: Можно ли создать абстрактному классу виртуальный метод? 
+// И вообще, можно ли абстрактному классу создать метод с телом?
+// Можно ли запечатать метод внутри обычного класса?
