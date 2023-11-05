d24eaa2838ee5007acb3774e487846c9fde36262
Mrsuper125 <Mrsuper1221@gmail.com>
2023-11-05 19:05:37 +0300

    feat: make coordinates triangle's own vertices public readonly

--- Polygons/Vertices/TriangleVertex.cs
+++ Polygons/Vertices/TriangleVertex.cs
@@ -4,29 +4,36 @@ namespace Polygons;
 
 class TriangleVertex: Shape
 {
+    public readonly double TopVertexX;
+    public readonly double TopVertexY;
+    public readonly double LeftVertexX;
+    public readonly double LeftVertexY;
+    public readonly double RightVertexX;
+    public readonly double RightVertexY;
+
     public TriangleVertex(double x, double y) : base(x, y)
-    {}
+    {
+        TopVertexX = this.x;
+        TopVertexY = this.y + VertexRadius;
+        LeftVertexX = this.x - Math.Cos(0.523599d) * VertexRadius;
+        LeftVertexY = this.y - Math.Sin(0.523599d) * VertexRadius;
+        RightVertexX = this.x + Math.Cos(0.523599d) * VertexRadius;
+        RightVertexY = LeftVertexY;
+    }
 
     public override bool IsInside(double x, double y)
     {
-        double topVertexX = this.x;
-        double topVertexY = this.y + VertexRadius;
-        double leftVertexX = this.x - Math.Cos(0.523599d) * VertexRadius;  //30 градусов - половина угла треугольника, гипотенуза которого = радиус описанной
-        double leftVertexY = this.y - Math.Sin(0.523599d) * VertexRadius;  //Радианы вместо градусов. Спрашивается, нафига? Это шарп, ничего не попишешь.
-        double rightVertexX = this.x + Math.Cos(0.523599d) * VertexRadius;  //30 градусов - половина угла треугольника, гипотенуза которого = радиус описанной
-        double rightVertexY = leftVertexY;
-
-        if (!((y - leftVertexY)/(topVertexY - leftVertexY) < (x - leftVertexX)/(topVertexX - leftVertexX)))  //длинное страншное неравенство, проверяющее нахождение точки под прямой. Инвертируется для того, чтобы отловить неподходящие точки
+        if (!((y - LeftVertexY)/(TopVertexY - LeftVertexY) < (x - LeftVertexX)/(TopVertexX - LeftVertexX)))  //длинное страншное неравенство, проверяющее нахождение точки под прямой. Инвертируется для того, чтобы отловить неподходящие точки
         {
             return false;
         }
         
-        if (!((y - rightVertexY)/(topVertexY - rightVertexY) < (x - rightVertexX)/(topVertexX - rightVertexX)))  //длинное страншное неравенство, проверяющее нахождение точки под прямой. Инвертируется для того, чтобы отловить неподходящие точки
+        if (!((y - RightVertexY)/(TopVertexY - RightVertexY) < (x - RightVertexX)/(TopVertexX - RightVertexX)))  //длинное страншное неравенство, проверяющее нахождение точки под прямой. Инвертируется для того, чтобы отловить неподходящие точки
         {
             return false;
         }
 
-        if (y < rightVertexY)
+        if (y < RightVertexY)
         {
             return false;
         }
