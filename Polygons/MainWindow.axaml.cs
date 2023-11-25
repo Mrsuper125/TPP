71012dbd82bcbc06712a8c2d42fccce4b6019f1f
Mrsuper125 <Mrsuper1221@gmail.com>
2023-11-25 15:45:42 +0300

    fix: clicking on UI doesn't trigger draw-related functions anymore

--- Polygons/MainWindow.axaml.cs
+++ Polygons/MainWindow.axaml.cs
@@ -1,14 +1,22 @@
 using Avalonia.Controls;
 using System;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace Polygons;
 
 public partial class MainWindow : Window
 {
+    public bool IsClickOnUI;    //Костыльная переменная, в которую пишется true, если клик пришёл по чему угодно, кроме рисовалки. TODO: спросить Завра про идеи получше
+    
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    private void Menu_OnClick(object? sender, PointerPressedEventArgs e)
+    {
+        IsClickOnUI = true;         //Пока что общий плейсхолдерный перехватчик UI-шных кликов
     }
     
     private void Win_PointerPressed(object sender, Avalonia.Input.PointerPressedEventArgs e)
@@ -20,29 +28,36 @@ public partial class MainWindow : Window
         // нам нужен объект (переменная), ссылающийся на наш контрол.
         // для этого воспользуемся методом Find и найдем его по имени myCC
 
-        
-        if (e.GetCurrentPoint(null).Properties.IsLeftButtonPressed)
+        if (!IsClickOnUI)       //Перехватываем UI-шные клики и вызываем методы клика только когда клик не по UI
         {
-            cc.LeftPointerPressed (e.GetPosition(cc).X, e.GetPosition(cc).Y);
-            
-            // при помощи GetPosition приводим координаты курсора 
-            // в систему координат нашего контрола для рисования
-            // передав его (cc) в качестве параметра
-            
-            // А затем просто вызываем созданный выше метод PointerPressed, 
-            // относящийся уже не к окну, а к контролу – там мы сможем рисовать
+            if (e.GetCurrentPoint(null).Properties.IsLeftButtonPressed)
+            {
+                cc.LeftPointerPressed(e.GetPosition(cc).X, e.GetPosition(cc).Y);
 
+                // при помощи GetPosition приводим координаты курсора 
+                // в систему координат нашего контрола для рисования
+                // передав его (cc) в качестве параметра
+
+                // А затем просто вызываем созданный выше метод PointerPressed, 
+                // относящийся уже не к окну, а к контролу – там мы сможем рисовать
+
+            }
+
+            if (e.GetCurrentPoint(null).Properties.IsRightButtonPressed)
+            {
+                cc.RigthPointerPressed(e.GetPosition(cc).X, e.GetPosition(cc).Y);
+            }
         }
-        if (e.GetCurrentPoint(null).Properties.IsRightButtonPressed)
+        else
         {
-            cc.RigthPointerPressed(e.GetPosition(cc).X, e.GetPosition(cc).Y);
+            IsClickOnUI = false;        //Если клик пришёл по UI, поняли, нифига не делаем, только следующий чур будет какой надо
         }
     }
 
     private void Win_PointerMoved(object? sender, PointerEventArgs e)       //Как и предыдущий, ловит событие (здесь - перемещение курсора) и прокидывает координаты в DrawingControl
     {
         DrawingControl cc = this.Find<DrawingControl>("MyDrawingControl");
-        cc.PointerMoved(e.GetPosition(cc).X, e.GetPosition(cc).Y);
+        cc.PointerMoved(e.GetPosition(cc).X, e.GetPosition(cc).Y);          //Здесь и в Released костыль с IsClickOnUI не засунут для экономии места в коде - если уже перехватили клики, эти 2 метода ничего не сделают
     }
     
     private void Win_PointerReleased(object? sender, PointerReleasedEventArgs e)    //Та же аналогия
